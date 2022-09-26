4d10782bbb95768fc6f92ef8f0965dce0ac4e5d3
Erik <erikhau3010@gmail.com>
2022-09-26 17:10:42 +0700

    feat: add animation in starter page

--- src/main.js
+++ src/main.js
@@ -4,49 +4,41 @@ import Game from "./Game";
 const buttonStart = document.querySelector("#btn-start");
 const logoElement = document.querySelector("#logo");
 const canvas = document.querySelector("#canvas");
+const starterWrapper = document.querySelector(".starter-wrapper");
 
 const game = new Game({ canvas });
-game.init();
 
-buttonStart.addEventListener("click", () => {
-  animate({
-    element: buttonStart,
-    animationClass: ["fade-out", "scale-out"],
-    type: "transition",
+const startHandler = async () => {
+  await Promise.all([
+    animate(logoElement, ["scale-out"]),
+    animate(buttonStart, ["fade-out"]),
+  ]);
+  logoElement.style.visibility = "hidden";
+  buttonStart.style.visibility = "hidden";
+
+  await animate(starterWrapper, ["active"]);
+  // starterWrapper.style.top = "15%";
+
+  canvas.style.visibility = "visible";
+  await animate(canvas, ["show"]);
+
+  game.init();
+};
+
+buttonStart.addEventListener("click", startHandler);
+
+const animate = (element, classList) => {
+  return new Promise((resolve) => {
+    const classes = ["animate", ...classList];
+
+    element.classList.add(...classes);
+
+    const animationCallback = () => {
+      // element.classList.remove(...classes);
+      element.removeEventListener("transitionend", animationCallback);
+      return resolve();
+    };
+
+    element.addEventListener("transitionend", animationCallback);
   });
-  // animate({
-  //   element: logoElement,
-  //   animationClass: ["fade-out", "scale-out"],
-  //   type: "transition",
-  // });
-
-  animate({
-    element: canvas,
-    animationClass: ["fade-in", "scale-in"],
-    type: "transition",
-    animationType: "in",
-  });
-});
-
-const animate = ({
-  element,
-  animationClass,
-  type = "animation",
-  animationType = "out",
-}) => {
-  const classes = ["animate", ...animationClass];
-
-  if (animationType === "in") element.classList.remove("hide");
-
-  element.classList.add(...classes);
-
-  const animationCallback = () => {
-    if (animationType === "out") {
-      element.classList.remove(...classes);
-      element.classList.add("hide");
-    }
-
-    element.removeEventListener(`${type}end`, animationCallback);
-  };
-  element.addEventListener(`${type}end`, animationCallback);
 };
